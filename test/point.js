7bcb08ffcfc8fc9e49ea1b99004f3c46704114ec
Chris Buttery <info@chrisbuttery.com>
2015-05-12 20:44:49 +1000

    Update tests for the Point component

--- test/point.js
+++ test/point.js
@@ -1,7 +1,9 @@
+import Point from '../lib/point';
 var assert = require('assert');
 
-describe('header', function(){
-  it('should do something amazing', function(){
-    assert.equal('1', '1');
+describe('Point', function(){
+  it('should do return a point value', function(){
+    let point = new Point(1, 2);
+    assert.equal(point, '(1, 2)');
   });
 });
